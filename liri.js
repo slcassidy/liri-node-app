89350378b032b01f89d7fdfc6af6a6dd9367de58
slcassidy <samantha.l.cassidy@gmail.com>
2019-04-03 22:58:47 -0400

    Upload 20190403

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 const axios = require('axios');
 // const inquirer = require('inquirer');
 
+
 require('dotenv').config();
 
  
@@ -12,87 +13,23 @@ if (process.argv.length > 2){
    view += `${process.argv[i]}`
   }
 }
-console.log(view)
+// console.log(view)
 const searchType = process.argv[2];
 
+if(searchType === `movie-this` && !view){  
+  view = "Mr Nobody"
+}else if(searchType === `spotify-this-song` && !view) {
+ view = "What's My Age Again"
+}
 
 
 // **********************OMDB Movie info****************************
 // const omdbKey = '54ed6069'
 const queryOmdb = `http://www.omdbapi.com/?t=${view}&apikey=${process.env.OMDBKEY}`
 
-// *********************Spotify info*********************
-const spot = function (){
-// Get the node file
-var Spotify = require('node-spotify-api');
-  // Password info
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET  
-});
-console.log (spotify);
-console.log(view)
- 
-spotify.search({ type: 'track', query: `${view}`})
-
- .then(function(response) {
-  const artist = response.tracks.items[1].album.artists[0].name
-  const song = response.tracks.items[1].album.name
-  const link = response.tracks.items[1].album.external_urls.spotify
-  const album = response.tracks.items[1].name
-  console.log(`Artist: ${artist}`);
-  console.log(`Name of Song: ${song}`);
-  console.log(`Preview Link: ${link}`);
-  console.log(`Album: ${album}`);
-
- 
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-
-} //End of spot function
-
-// GET `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_ID}&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09`
-
-// ******************************Band Info*******************************
-// See information @`http://www.artists.bandsintown.com/bandsintown-api`
-
-const queryBand = `https://rest.bandsintown.com/artists/${view}/events?app_id=${process.env.BANDKEY}`
-
-// https://rest.bandsintown.com/artists/jason%20mraz/events?app_id=dsdf&date=4%2F1%2F2019
-  
-//*******************Variables*********************** 
-    let select = "";
-
-    console.log(view)
-    console.log(searchType)
-
-
-// Case switch to identify which API to call
-
-
-      switch(searchType){
-        case `movie-this`:
-        select = queryOmdb;
-        break;
-        case 'concert-this':
-        select = queryBand;
-        // console.log(queryBand);
-        break;
-        case 'spotify-this-song':
-        spot();
-        break;
-        case 'do-what-it-says':
-        spot();
-        break;
-        default:        
-        console.log ("Not Available")
-      };
-  
-      console.log(select);
-     
-axios.get(select).then(function(response) {
+// console.log(queryOmdb)
+const movie = function(){
+  axios.get(queryOmdb).then(function(response) {
 
     const title = response.data.Title
     const year = response.data.Year
@@ -104,7 +41,7 @@ axios.get(select).then(function(response) {
     const lang = response.data.Language
     const plot = response.data.Plot
 
-    const movieInfo = function(response){
+    const movieInfo = function(){
       console.log(`Movie Title: ${title}`);
       console.log(`Year: ${year}`);
       console.log(`Country filmed: ${country}`);
@@ -118,34 +55,143 @@ axios.get(select).then(function(response) {
     
     // If the request was successful...
     if (response.status === 200) {    
-      // Then log the data from the site!
-      
-      if (select = queryOmdb){
-        movieInfo();
-      } else {
-         console.log(response.data);
-      }
-
-      
-      // console.log(response.data);
+      movieInfo();
     }    
   }); //Edn of axios
+}//End of Then function
+
+
+
+// ******************************Band Info*******************************
+// See information @`http://www.artists.bandsintown.com/bandsintown-api`
+
+const queryBand = `https://rest.bandsintown.com/artists/${view}/events?app_id=${process.env.BANDKEY}`
+
+
+const band = function(){
+  axios.get(queryBand).then(function(response) {
+
+    const vname = response.data[0].venue.name
+    const country = response.data[0].venue.country
+    const city = response.data[0].venue.city
+    const region = response.data[0].venue.region
+ 
+
+
+    const bandInfo = function(){
+      console.log(`Venue Name: ${vname}`);
+      console.log(`Location: ${city}, ${region}, ${country}`);   
+    }
+
+
+    // console.log(queryBand)
+    
+    // If the request was successful...
+    if (response.status === 200) {  
+        bandInfo();
+    }    
+  }); //Edn of axios
+}//End of Then function
+
+ // *********************Spotify info*********************
+const spot = function (view){
+  // Get the node file
+  var Spotify = require('node-spotify-api');
+    // Password info
+  var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET  
+  });
+  // console.log (spotify);
+  // console.log(view)
+   
+  spotify.search({ type: 'track', query: `${view}`})
   
+   .then(function(response) {
+    const artist = response.tracks.items[1].album.artists[0].name
+    const song = response.tracks.items[1].album.name
+    const link = response.tracks.items[1].album.external_urls.spotify
+    const album = response.tracks.items[1].name
+    console.log(`Artist: ${artist}`);
+    console.log(`Name of Song: ${song}`);
+    console.log(`Preview Link: ${link}`);
+    console.log(`Album: ${album}`);
+  
+   
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+  
+  } //End of spot function
 
- //End of Then function
-
-// TURN BACK ON BOTTOM
+  // do what it says area
 
 
 
+  // Includes the FS package for reading and writing packages
+
+const doit = function(searchType, view){
+
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable 'data'
+fs.readFile('random.txt', 'utf8', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // Break the string down by comma separation and store the contents into the output array.
+  const output = data.split(',');
+
+  searchType = output[0];
+  view = output[1];
+  // console.log(view);
+  // console.log(searchType);
+  spot(view);
+
+});
+}
+
+// write to file
+const logInfo = function () {
+  const fs = require('fs');
+  // We will add the value to the bank file.
+  fs.appendFile('log.txt', `${searchType}`, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+
+  // fs.appendFile('log.txt', `${view} ,`, function(err) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  // });
+  // We will then print the value that was added (but we wont print the total).
+  console.log(`Info on log ${searchType} & ${view}.`);
+}
 
 
-// // Run the get method for the OMDB to identify Movies
-// axios.get(queryOmdb).then(function(response) {
+// Case switch to identify which API to call
+logInfo();
 
-//   // If the request was successful...
-//   if (response.status === 200) {
+switch(searchType){
+  case `movie-this`:
+  movie();
+  break;
+  case 'concert-this':
+  band();
+  break;
+  case 'spotify-this-song':
+  spot();
+  break;
+  case 'do-what-it-says':
+  doit();
+  break;
+  default:        
+  console.log ("Not Available")
+};
 
-//     // Then log the data from the site!
-//     console.log(response.data);
-//   }
+
+
+
